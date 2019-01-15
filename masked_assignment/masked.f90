b7c4602ee099c371e12a42f4ccc513dd8d6989c8
Alessandro Fanfarillo <elfanfa@ucar.edu>
2019-01-15 11:48:20 -0700

    More examples about masked_assignment

--- masked_assignment/masked.f90
+++ masked_assignment/masked.f90
@@ -3,8 +3,8 @@ program masked
   use iso_fortran_env, only : real64
   implicit none
   
-  integer, parameter :: n = 100000
-  integer :: counter, i
+  integer, parameter :: n = 50000000
+  integer :: i
   real(real64), allocatable :: a(:), b(:),c(:)
 
   allocate(a(n),b(n),c(n))
@@ -12,22 +12,19 @@ program masked
   b = (/ (i, i = 1, n) /)
 
   c = 0.0
-  counter = 0
 
   do i = 1, n
      a(i) = i + 2.0
   end do
 
-  call where_all(a,b,c)
-  write(*,*) c(10)
-  c = 0.0
-  call where_sqrt(a,b,c)
-  write(*,*) c(10)
-  c = 0.0
-  c(1000) = 1.0
-  call do_counter_partial_sum(c,counter)
-  call do_counter_count(c,counter)
-  write(*,*) counter
+  ! call where_all(a,b,c)
+  ! write(*,*) c(10)
 
+  call if_then_all(a,b,c)
+  write(*,*) c(10)
+
+  ! c = 0.0
+  !call where_sqrt(a,b,c)
+  !write(*,*) c(10)
 
 end program masked
