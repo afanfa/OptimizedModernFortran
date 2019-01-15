fd31332eedb9f89d63f81d7016a897a87af84eef
Alessandro Fanfarillo <elfanfa@ucar.edu>
2019-01-15 14:50:06 -0700

    New noise test

---
+++ numerical_noise/noise.f90
@@ -0,0 +1,25 @@
+program noise
+  use iso_fortran_env, only : real32
+  implicit none
+
+  integer, parameter :: n = 100000
+  real(real32), allocatable :: a(:),b(:)
+  real(real32) :: redux
+  integer :: i
+
+  allocate(a(n),b(n))
+
+  do i = 1, n
+     a(i) = i + 314.1415
+     b(i) = 1/(1+10000*i)
+  end do
+
+  redux = 0.d0
+!dir$ novector
+  do i = 1, n
+     redux = redux + a(i) + b(i)
+  end do
+
+  write( *, "(F24.8)") redux
+
+end program noise
