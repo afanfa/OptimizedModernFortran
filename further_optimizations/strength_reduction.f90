6ce4d22fa56bfa1ab4bedf2a6ce2b220a4bf2ec2
Alessandro Fanfarillo <elfanfa@ucar.edu>
2019-01-22 13:56:12 -0700

    Strength reduction test

---
+++ further_optimizations/strength_reduction.f90
@@ -0,0 +1,28 @@
+program strength_red
+  use iso_fortran_env, only : real64
+  implicit none
+
+  integer, parameter :: n = 19900000
+  integer :: i,j
+  real(real64), dimension(n) :: a,b
+  real(real64) :: rec, pi = 3.1415
+
+  a = (/ (i, i = 1, n) /)
+  b = (/ (2*i, i = 1, n) /)
+
+  do j = 1, 1000
+     do i = 1, n
+        a(i) = b(i) / pi
+     end do
+  end do
+  ! rec = 1/pi
+
+  ! do j = 1, 1000
+  !    do i = 1, n
+  !       a(i) = b(i) * rec
+  !    end do
+  ! end do
+  
+  write(*,*) a(100)
+
+end program strength_red
