64035515fd4b9c2ccd55c84754b453c0973fe9a8
Alessandro Fanfarillo <elfanfa@ucar.edu>
2019-01-22 15:45:56 -0700

    Minor changes

--- further_optimizations/strength_reduction.f90
+++ further_optimizations/strength_reduction.f90
@@ -5,7 +5,7 @@ program strength_red
   integer, parameter :: n = 19900000
   integer :: i,j
   real(real64), dimension(n) :: a,b
-  real(real64) :: rec, pi = 3.1415
+  real(real64) :: rec, pi = 3.1415, r
 
   a = (/ (i, i = 1, n) /)
   b = (/ (2*i, i = 1, n) /)
@@ -15,6 +15,7 @@ program strength_red
         a(i) = b(i) / pi
      end do
   end do
+
   ! rec = 1/pi
 
   ! do j = 1, 1000
