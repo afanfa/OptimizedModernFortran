5aaafc7b46534f5682947c89c5b35a9f810e75e5
Alessandro Fanfarillo <elfanfa@ucar.edu>
2019-07-22 07:13:03 -0600

    Minor change to do_concurrent example

--- do_concurrent/vectorization.f90
+++ do_concurrent/vectorization.f90
@@ -3,13 +3,18 @@ program vector_dep
   use iso_fortran_env, only : real32
   implicit none
 
-  integer, parameter :: n = 1000000
+  integer, parameter :: n = 10000000
   integer :: i,k=1
   real(real32),dimension(n) :: a
   real(real32) :: c
 
   a = (/ (i, i = 1, n ) /)
+  c = 0.5
 
   call vec_dep(a,c,k,n)
+  !call vec_dep_dir(a,c,k,n)
+  !call vec_dep_do_concurrent(a,c,k,n)
+
+  write(*,*) a(10)
 
 end program vector_dep
